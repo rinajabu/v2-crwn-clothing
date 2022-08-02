0828cf77a34013d5f9964c87d5291bcb91432cc1
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro-2.local>
2022-08-01 22:36:43 -0400

    custom button component created and styled

--- src/components/sign-up-form/SignUpForm.component.jsx
+++ src/components/sign-up-form/SignUpForm.component.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import Button from '../button/Button.component'
 import FormInput from '../form-input/FormInput.component'
+import './sign-up-form.styles.scss'
 
 const defaultFormFields = {
     displayName: '',
@@ -46,8 +48,9 @@ const SignUpForm = () => {
     }
 
     return (
-        <div>
-            <h1>Sign up with you email and password</h1>
+        <div className='sign-up-container'>
+            <h2>Don't have an account?</h2>
+            <span>Sign up with you email and password</span>
             <form onSubmit={handleSubmit}>
                 
                 <FormInput 
@@ -86,7 +89,7 @@ const SignUpForm = () => {
                     required 
                 />
 
-                <button type="submit">Sign Up</button>
+                <Button type="submit">Sign Up</Button>
             </form>
         </div>
     )
